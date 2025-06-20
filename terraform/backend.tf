205cc1046f2bf791a8aaac9041dd2327c7be2c47
Prasadrasal2002 <145026242+Prasadrasal2002@users.noreply.github.com>
2025-06-20 10:07:25 +0530

    Delete terraform directory

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "interior-web-app-9866"
-    key            = "dev/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "interior-dynamodb-2025"
-    encrypt        = true
-  }
-}
